e43796ceeda72909d84bae1a6ca154396c1d7060
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-10-14 02:02:44 +0900

    4.1 所有権と関数

--- README.md
+++ README.md
@@ -69,3 +69,30 @@ println!("{}, world!", s1); // error
 
 - クローン
   - ヒープデータのdeep copyが必要ならば、`clone`を使う
+- 関数に値を渡したり、関数の戻り値を受け取ったりすることで、所有権は移動する
+
+```rust
+fn main() {
+    let s = String::from("hello");  // sがスコープに入る
+
+    takes_ownership(s);             // sの値が関数にムーブされ...
+                                    // ... ここではもう有効ではない
+
+    let x = gives_ownership();
+
+    println!("{}", x);
+    // println!("{}", s);  // ここでsを参照するとエラー
+
+} // ここでs,xがスコープを抜ける。xはドロップされるが、sの値はムーブされてるので、何も特別なことはない。
+
+fn takes_ownership(some_string: String) { // some_stringがスコープに入る。
+    println!("{}", some_string);
+} // ここでsome_stringがスコープを抜け、`drop`が呼ばれる。後ろ盾してたメモリが解放される。
+  // 
+
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+
+    some_string  // 呼び出し元関数にsome_stringがムーブされる
+}
+```
--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,14 +1,22 @@
 fn main() {
-    let s = "hello";
-    println!("Hello, world!");
-    
-    // move
-    let s1 = String::from("hello");
-    let s2 = s1;
-    // println!("{}, world!", s1); // error
+    let s = String::from("hello");  // sがスコープに入る
 
-    // clone
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
-    println!("s1 = {}, s2 = {}", s1, s2);
+    takes_ownership(s);             // sの値が関数にムーブされ...
+                                    // ... ここではもう有効ではない
+
+    let x = gives_ownership();
+
+    println!("{}", x);
+    // println!("{}", s);  // error
+} // ここでs,xがスコープを抜ける。xはドロップされるが、sの値はムーブされてるので、何も特別なことはない。
+
+fn takes_ownership(some_string: String) { // some_stringがスコープに入る。
+    println!("{}", some_string);
+} // ここでsome_stringがスコープを抜け、`drop`が呼ばれる。後ろ盾してたメモリが解放される。
+  // 
+
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+
+    some_string  // 呼び出し元関数にsome_stringがムーブされる
 }
