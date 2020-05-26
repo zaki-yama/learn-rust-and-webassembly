fbbde71670627839950d7483bbf17680f69b8502
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-05-27 01:25:16 +0900

    [p139] 3-6 最終形:並列ソートの実現

--- rustbook/ch03/bitonic-sorter/src/fourth.rs
+++ rustbook/ch03/bitonic-sorter/src/fourth.rs
@@ -4,7 +4,8 @@ use std::cmp::Ordering;
 
 pub fn sort_by<T, F>(x: &mut [T], comparator: &F) -> Result<(), String>
 where
-  F: Fn(&T, &T) -> Ordering,
+  T: Send,
+  F: Sync + Fn(&T, &T) -> Ordering,
 {
   if x.len().is_power_of_two() {
     do_sort(x, true, comparator);
@@ -17,7 +18,7 @@ where
   }
 }
 
-pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) -> Result<(), String> {
+pub fn sort<T: Ord + Send>(x: &mut [T], order: &SortOrder) -> Result<(), String> {
   match *order {
     SortOrder::Ascending => sort_by(x, &|a, b| a.cmp(b)),
     SortOrder::Descending => sort_by(x, &|a, b| b.cmp(a)),
@@ -28,17 +29,19 @@ const PARALLEL_THRESHOLD: usize = 4096;
 
 fn do_sort<T, F>(x: &mut [T], forward: bool, comparator: &F)
 where
-  F: Fn(&T, &T) -> Ordering,
+  T: Send,
+  F: Sync + Fn(&T, &T) -> Ordering,
 {
   if x.len() > 1 {
     let mid_point = x.len() / 2;
 
+    let (first, second) = x.split_at_mut(mid_point);
     // xの分割後の要素数をしきい値(PARALLEL_THRESHOLD)と比較する
     if mid_point >= PARALLEL_THRESHOLD {
       // しきい値以上なら並列にソートする(並列処理)
       rayon::join(
-        || do_sort(&mut x[..mid_point], true, comparator),
-        || do_sort(&mut x[mid_point..], false, comparator),
+        || do_sort(first, true, comparator),
+        || do_sort(second, false, comparator),
       );
     } else {
       do_sort(&mut x[..mid_point], true, comparator);
@@ -50,13 +53,22 @@ where
 
 fn sub_sort<T, F>(x: &mut [T], forward: bool, comparator: &F)
 where
-  F: Fn(&T, &T) -> Ordering,
+  T: Send,
+  F: Sync + Fn(&T, &T) -> Ordering,
 {
   if x.len() > 1 {
     compare_and_swap(x, forward, comparator);
     let mid_point = x.len() / 2;
-    sub_sort(&mut x[..mid_point], forward, comparator);
-    sub_sort(&mut x[mid_point..], forward, comparator);
+    let (first, second) = x.split_at_mut(mid_point);
+    if mid_point >= PARALLEL_THRESHOLD {
+      rayon::join(
+        || sub_sort(first, forward, comparator),
+        || sub_sort(second, forward, comparator),
+      );
+    } else {
+      sub_sort(&mut x[..mid_point], forward, comparator);
+      sub_sort(&mut x[mid_point..], forward, comparator);
+    }
   }
 }
 
