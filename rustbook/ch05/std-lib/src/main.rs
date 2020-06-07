10b7e544da047a884b953417b3f82ff5bcdd6b53
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-06-07 14:51:49 +0900

    [p188] 5-2-5 範囲

--- rustbook/ch05/std-lib/src/main.rs
+++ rustbook/ch05/std-lib/src/main.rs
@@ -96,6 +96,25 @@ fn main() {
     // 不正なバイト列はUnnicodeのU+FFFD Replacement Characterに置き換わる
     let s = String::from_utf8_lossy(&bad_utf8);
     assert_eq!(s, "a\u{fffd}あ");
+
+    // --- 5-2-5 範囲
+    let a = ['a', 'b', 'c', 'd', 'e'];
+
+    // 糖衣構文と実際の範囲の対応
+    assert_eq!(a[..], ['a', 'b', 'c', 'd', 'e']);
+    assert_eq!(a[..3], ['a', 'b', 'c']);
+    assert_eq!(a[..=3], ['a', 'b', 'c', 'd']);
+    assert_eq!(a[1..], ['b', 'c', 'd', 'e']);
+    assert_eq!(a[1..3], ['b', 'c']);
+    assert_eq!(a[1..=3], ['b', 'c', 'd']);
+
+    // 糖衣構文とRange*型の対応
+    assert_eq!(.., std::ops::RangeFull);
+    assert_eq!(..3, std::ops::RangeTo { end: 3 });
+    assert_eq!(..=3, std::ops::RangeToInclusive { end: 3 });
+    assert_eq!(1.., std::ops::RangeFrom { start: 1 });
+    assert_eq!(1..3, std::ops::Range { start: 1, end: 3 });
+    assert_eq!(1..=3, std::ops::RangeInclusive::new(1, 3));
 }
 
 // 間違った戻り値の型
