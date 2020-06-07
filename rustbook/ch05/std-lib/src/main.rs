b07baf14e907b9e311722a561173dc545a63072e
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-06-07 13:37:51 +0900

    [p188] 5-2-4 String

--- rustbook/ch05/std-lib/src/main.rs
+++ rustbook/ch05/std-lib/src/main.rs
@@ -44,4 +44,69 @@ fn main() {
     let d = m1.entry("d").or_insert(0);
     *d += 7;
     assert_eq!(m1.get("d"), Some(&7));
+
+    // --- 5-2-4 String
+    // strリテラルからStringを作る。どちらの方法でも結果は同じ
+    let mut s1 = "ラズベリー".to_string();
+    let mut s2 = String::from("ブラックベリー");
+
+    s1.push_str("タルト");
+    assert_eq!(s1, "ラズベリータルト");
+
+    s2.push('と');
+
+    // push_str()が受け付けるのは&str型のみ。以下はコンパイルエラーになる
+    let s3 = String::from("ストロベリー");
+    // s2.push_str(s3); // error
+
+    // &をつけると型強制というしくみによって&Stringから&strへ変換される
+    s2.push_str(&s3);
+    assert_eq!(s2, "ブラックベリーとストロベリー");
+
+    let i = 42;
+    assert_eq!(i.to_string(), "42");
+
+    let f = 4.3 + 0.1;
+    assert_eq!(f.to_string(), "4.3999999999999995");
+    assert_eq!(format!("{:.2}", f), "4.40");
+
+    let t = (1, "ABC");
+    // 2要素のタプル型はDebugトレイトを実装しているのでformat!マクロで変換できる
+    assert_eq!(format!("{:?}", t), r#"(1, "ABC")"#);
+
+    let s1 = "42";
+    assert_eq!(s1.parse::<i32>(), Ok(42)); // &str型からi32型へ変換
+
+    let s2 = "abc";
+    // 変数の型から推論できるならparseの型パラメータは不要
+    let r2: Result<f64, _> = s2.parse();
+    assert!(r2.is_err());
+    println!("{:?}", r2);
+
+    let cs = ['t', 'r', 'u', 's', 't']; // [char; 5]
+    assert_eq!(cs.iter().collect::<String>(), "trust");
+    assert_eq!(&cs[1..].iter().collect::<String>(), "rust");
+
+    let bad_utf8: [u8; 7] = [
+        b'a', // a
+        0xf0, 0x90, 0x80, // でたらめなバイト列
+        0xe3, 0x81, 0x82, // あ
+    ];
+
+    // 不正なバイト列はUnnicodeのU+FFFD Replacement Characterに置き換わる
+    let s = String::from_utf8_lossy(&bad_utf8);
+    assert_eq!(s, "a\u{fffd}あ");
+}
+
+// 間違った戻り値の型
+// fn f1(name: &str) -> &str {
+//    let s = format!("Hello, {}!", name);
+//    Stringから&strを作成し、戻り値として返す
+//    // コンパイルエラー
+//    &s
+// }
+
+// 正しい戻り値の型
+fn f1(name: &str) -> String {
+    format!("Hello, {}!", name)
 }
