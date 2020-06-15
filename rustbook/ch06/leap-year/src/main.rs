d8d566e7d6def301bb6ec7e9c448496352ababb9
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-06-16 02:22:15 +0900

    [p223] 6-5-3 メソッド, 6-5-4 関連関数

--- rustbook/ch06/leap-year/src/main.rs
+++ rustbook/ch06/leap-year/src/main.rs
@@ -13,8 +13,29 @@ fn main() {
     } else {
         println!("{} is not a leap year.", year);
     }
+
+    // --- 6-5-3 メソッド, 6-5-4 関連関数
+    let circle1 = Circle { radius: 10 };
+    println!("Circle1's diameter: {}", circle1.diameter());
+    let circle1 = Circle::small_circle();
+    println!("Circle1's diameter: {}", circle1.diameter());
 }
 
 fn is_leap_year(year: u32) -> bool {
     year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0)
+}
+
+// --- 6-5-3 メソッド, 6-5-4 関連関数
+struct Circle {
+    radius: u32,
+}
+
+impl Circle {
+    fn diameter(&self) -> u32 {
+        self.radius * 2
+    }
+
+    fn small_circle() -> Circle {
+        Circle { radius: 1 }
+    }
 }
