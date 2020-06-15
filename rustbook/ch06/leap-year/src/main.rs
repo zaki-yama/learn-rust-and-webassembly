2770eac1a3f26c284ea426472b9e484df25135ed
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-06-15 09:15:01 +0900

    [p218] 6-3 うるう年と平年

--- rustbook/ch06/leap-year/src/main.rs
+++ rustbook/ch06/leap-year/src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut year = String::new();
+    print!("Please input a year to check if it is a leap year: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut year).unwrap();
+    let year = year.trim().parse::<u32>().unwrap();
+
+    if is_leap_year(year) {
+        println!("{} is a leap year!", year);
+    } else {
+        println!("{} is not a leap year.", year);
+    }
+}
+
+fn is_leap_year(year: u32) -> bool {
+    year % 4 == 0 && !(year % 100 == 0 && year % 400 != 0)
 }
