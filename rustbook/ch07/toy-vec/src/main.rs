6fef4327851682a52f2243249bcfd7912dc6aa0c
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-06-17 17:52:59 +0900

    [p271] 7-9-4 pushメソッドとgetメソッド

--- rustbook/ch07/toy-vec/src/main.rs
+++ rustbook/ch07/toy-vec/src/main.rs
@@ -33,6 +33,29 @@ impl<T: Default> ToyVec<T> {
     pub fn capacity(&self) -> usize {
         self.elements.len()
     }
+
+    pub fn push(&mut self, element: T) {
+        if self.len == self.capacity() {
+            self.grow();
+        }
+        self.elements[self.len] = element;
+        self.len += 1;
+    }
+
+    pub fn get(&self, index: usize) -> Option<&T> {
+        if index < self.len {
+            Some(&self.elements[index])
+        } else {
+            None
+        }
+    }
+
+    fn grow(&mut self) {
+        // TODO
+        // - self.capacityが0のときは、allocate_in_heap(1)で長さ1のBox<[T]>を作成しself.elementsにセット
+        // - self.capacityが1以上のときは、allocate_in_heap(self.capacity() * 2)で現在の2倍の長さのBox<[T]>を作成しself.elementsにセット
+        // 既存の全要素を新しいBox<[T]>にムーブしたあと、古いBox<[T]>を破棄する
+    }
 }
 
 fn main() {
