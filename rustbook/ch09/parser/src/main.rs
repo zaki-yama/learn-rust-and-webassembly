0440f227eb0c0cdd545af7fb4df4a83a5c111eec
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-07-03 10:25:14 +0900

    [p356] 9-2-1 トークン

---
+++ rustbook/ch09/parser/src/main.rs
@@ -0,0 +1,93 @@
+// 位置情報。.0から.1までの区間を表す
+// たとえばLoc(4, 6)なら入力文字の5文字目から7文字目までの区間を表す(0始まり)
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Loc(usize, usize);
+
+// loc に便利メソッドを実装しておく
+impl Loc {
+    fn merge(&self, other: &Loc) -> Loc {
+        use std::cmp::{max, min};
+        Loc(min(self.0, other.0), max(self.1, other.1))
+    }
+}
+
+// アノテーション。値にさまざまなデータをもたせたもの。ここではLocをもたせている
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Annot<T> {
+    value: T,
+    loc: Loc,
+}
+
+impl<T> Annot<T> {
+    fn new(value: T, loc: Loc) -> Self {
+        Self { value, loc }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum TokenKind {
+    /// [0-9][0-9]*
+    Number(u64),
+    Plus,
+    Minus,
+    Asterisk,
+    Slash,
+    /// (
+    LParen,
+    /// )
+    RParen,
+}
+
+type Token = Annot<TokenKind>;
+
+// ヘルパーメソッドを定義しておく
+impl Token {
+    fn number(n: u64, loc: Loc) -> Self {
+        Self::new(TokenKind::Number(n), loc)
+    }
+    fn plus(loc: Loc) -> Self {
+        Self::new(TokenKind::Plus, loc)
+    }
+
+    fn minus(loc: Loc) -> Self {
+        Self::new(TokenKind::Minus, loc)
+    }
+
+    fn asterisk(loc: Loc) -> Self {
+        Self::new(TokenKind::Asterisk, loc)
+    }
+
+    fn slash(loc: Loc) -> Self {
+        Self::new(TokenKind::Slash, loc)
+    }
+
+    fn lparen(loc: Loc) -> Self {
+        Self::new(TokenKind::LParen, loc)
+    }
+
+    fn rparen(loc: Loc) -> Self {
+        Self::new(TokenKind::RParen, loc)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum LexErrorKind {
+    InvalidChar(char),
+    Eof,
+}
+
+type LexError = Annot<LexErrorKind>;
+
+impl LexError {
+    fn invalid_char(c: char, loc: Loc) -> Self {
+        LexError::new(LexErrorKind::InvalidChar(c), loc)
+    }
+
+    fn eof(loc: Loc) -> Self {
+        LexError::new(LexErrorKind::Eof, loc)
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
