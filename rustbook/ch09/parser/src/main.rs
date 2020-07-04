0d6a6c8e6d9837e40bb953b7f56cb822c2d58677
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-07-04 20:13:20 +0900

    [p371] parse_expr3の途中: 左再帰問題が含まれる実装

--- rustbook/ch09/parser/src/main.rs
+++ rustbook/ch09/parser/src/main.rs
@@ -232,6 +232,173 @@ fn prompt(s: &str) -> io::Result<()> {
     stdout.flush()
 }
 
+/// ASTを表すデータ型
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum AstKind {
+    /// 数値
+    Num(u64),
+    /// 単項演算
+    UniOp { op: UniOp, e: Box<Ast> },
+    /// 二項演算
+    BinOp { op: BinOp, l: Box<Ast>, r: Box<Ast> },
+}
+
+type Ast = Annot<AstKind>;
+
+// ヘルパメソッドを定義しておく
+impl Ast {
+    fn num(n: u64, loc: Loc) -> Self {
+        // impl<T> Annot<T>で実装したnewを呼ぶ
+        Self::new(AstKind::Num(n), loc)
+    }
+
+    fn uniop(op: UniOp, e: Ast, loc: Loc) -> Self {
+        Self::new(AstKind::UniOp { op, e: Box::new(e) }, loc)
+    }
+
+    fn binop(op: BinOp, l: Ast, r: Ast, loc: Loc) -> Self {
+        Self::new(
+            AstKind::BinOp {
+                op,
+                l: Box::new(l),
+                r: Box::new(r),
+            },
+            loc,
+        )
+    }
+}
+
+/// 単項演算子を表すデータ型
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum UniOpKind {
+    /// 正号
+    Plus,
+    /// 負号
+    Minus,
+}
+
+type UniOp = Annot<UniOpKind>;
+
+impl UniOp {
+    fn plus(loc: Loc) -> Self {
+        Self::new(UniOpKind::Plus, loc)
+    }
+
+    fn minus(loc: Loc) -> Self {
+        Self::new(UniOpKind::Minus, loc)
+    }
+}
+
+/// 二項演算子を表すデータ型
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum BinOpKind {
+    /// 加算
+    Add,
+    /// 減算
+    Sub,
+    /// 乗算
+    Mult,
+    /// 除算
+    Div,
+}
+
+type BinOp = Annot<BinOpKind>;
+
+impl BinOp {
+    fn add(loc: Loc) -> Self {
+        Self::new(BinOpKind::Add, loc)
+    }
+    fn sub(loc: Loc) -> Self {
+        Self::new(BinOpKind::Sub, loc)
+    }
+    fn mult(loc: Loc) -> Self {
+        Self::new(BinOpKind::Mult, loc)
+    }
+    fn div(loc: Loc) -> Self {
+        Self::new(BinOpKind::Div, loc)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum ParseError {
+    /// 予期しないトークンがきた
+    UnexpectedToken(Token),
+    /// 式を期待していたのに式でないものがきた
+    NotExpression(Token),
+    /// 演算子を期待していたのに演算子でないものがきた
+    NotOperator(Token),
+    /// 括弧が閉じられていない
+    UnclosedOpenParen(Token),
+    /// 式の解析が終わったのにまだトークンが残っている
+    RedundantExpression(Token),
+    /// パース途中で入力が終わった
+    Eof,
+}
+
+fn parse(tokens: Vec<Token>) -> Result<Ast, ParseError> {
+    // 入力をイテレータにし、Peekableにする
+    let mut tokens = tokens.into_iter().peekable();
+    // その後parse_exprを呼んでエラー処理をする
+    let ret = parse_expr(&mut tokens)?;
+    match tokens.next() {
+        Some(tok) => Err(ParseError::RedundantExpression(tok)),
+        None => Ok(ret),
+    }
+}
+
+fn parse_expr<Tokens>(tokens: &mut Peekable<Tokens>) -> Result<Ast, ParseError>
+where
+    Tokens: Iterator<Item = Token>,
+{
+    parse_expr3(tokens)
+}
+
+use std::iter::Peekable;
+/// EXPR3 = EXPR3, ("+" | "-"), EXPR2 | EXPR2 ;
+fn parse_expr3<Tokens>(tokens: &mut Peekable<Tokens>) -> Result<Ast, ParseError>
+where
+    Tokens: Iterator<Item = Token>,
+{
+    // 最初に `EXPR3 ("+" | "-") EXPR2` を試す
+    // まずはEXPR3をパースし
+    match parse_expr3(tokens) {
+        // 失敗したらparse_expr2にフォールバック (`| EXPR2` の部分)
+        Err(_) => parse_expr2(tokens),
+        // 成功したら
+        Ok(e) => {
+            // peekで先読みして
+            match tokens.peek().map(|tok| tok.value) {
+                // ("+" | "-") であることを確認する。| を使ってパターンマッチで複数並べられる
+                Some(TokenKind::Plus) | Some(TokenKind::Minus) => {
+                    // ("+" | "-") であれば入力を消費してパースを始める
+                    let op = match tokens.next().unwrap() {
+                        // Tokenは型エイリアスだがパターンマッチにも使える
+                        Token {
+                            // パターンマッチはネスト可能
+                            value: TokenKind::Plus,
+                            loc,
+                        } => BinOp::add(loc),
+                        Token {
+                            value: TokenKind::Minus,
+                            loc,
+                        } => BinOp::sub(loc),
+                        // 入力が"+"か"-"であることは確認したのでそれ以外はありえない
+                        _ => unreachable!(),
+                    };
+                    // EXPR2 をパース
+                    let r = parse_expr2(tokens)?;
+                    // 結果は加減
+                    let loc = e.loc.merge(&r.loc);
+                    Ok(Ast::binop(op, e, r, loc))
+                }
+                // それ以外はエラー。エラーの種類で処理を分ける
+                Some(_) => Err(ParseError::UnexpectedToken(tokens.next().unwrap())),
+                None => Err(ParseError::Eof),
+            }
+        }
+    }
+}
+
 fn main() {
     use std::io::{stdin, BufRead, BufReader};
 
