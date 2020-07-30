5b26eb51f65f62f7f8a94b73b8db34d242da8686
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-07-30 09:26:25 +0900

    14 テキストのロギング

--- tour-of-webassembly/index.js
+++ tour-of-webassembly/index.js
@@ -1,15 +1,20 @@
 async function run() {
-  let response = await fetch('../main.wasm');
+  let response = await fetch("../main.wasm");
   let bytes = await response.arrayBuffer();
   let module = await WebAssembly.instantiate(bytes, {
     env: {
-      get_magic_number() {
-        return 42;
-      }
-    }
+      wasm_log(start, len) {
+        // 開始位置と長さから、テキストを抽出する
+        const utf8dec = new TextDecoder("utf-8");
+        let buffer = module.instance.exports.memory.buffer;
+        let memory = new Uint8Array(buffer);
+        let text = utf8dec.decode(memory.subarray(start, start + len));
+        document.getElementById("container").innerHTML += text+"<br>";
+
+      },
+    },
   });
-  let num = module.instance.exports.main();
-  document.getElementById("container").textContent = num;
+  module.instance.exports.main();
 }
 
 run();
--- tour-of-webassembly/src/lib.rs
+++ tour-of-webassembly/src/lib.rs
@@ -5,9 +5,19 @@ pub fn add(a: i32, b: i32) -> i32 {
 
 extern "C" {
     fn get_magic_number() -> i32;
+    fn wasm_log(start: usize, len: usize);
 }
 
 #[no_mangle]
-pub fn main() -> i32 {
-    unsafe { get_magic_number() }
+pub fn main() {
+    log("hello world!");
+}
+
+// 14 テキストのロギング
+fn log(msg: &str) {
+    let start = msg.as_ptr();
+    let len = msg.len();
+    unsafe {
+        wasm_log(start as usize, len);
+    }
 }
