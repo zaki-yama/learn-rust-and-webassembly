0c098a6865d13d9323f177c955346ebd07ef20f4
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-08-27 09:44:47 +0900

    4.4 Implementing Life > Rust Impementation (Display, new, render)

--- rustwasm-book/wasm-game-of-life/src/lib.rs
+++ rustwasm-book/wasm-game-of-life/src/lib.rs
@@ -49,6 +49,7 @@ impl Universe {
     }
 }
 
+/// Public methods, exported to JavaScript.
 #[wasm_bindgen]
 impl Universe {
     pub fn tick(&mut self) {
@@ -81,5 +82,45 @@ impl Universe {
             }
         }
         self.cells = next;
+    }
+
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+}
+
+use std::fmt;
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
     }
 }
