22489b44e3902638ab9fe28370b48523a9d04120
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-09-02 09:23:29 +0900

    Debug無効に

--- rustwasm-book/wasm-game-of-life/src/lib.rs
+++ rustwasm-book/wasm-game-of-life/src/lib.rs
@@ -70,13 +70,13 @@ impl Universe {
                 let cell = self.cells[idx];
                 let live_neighbors = self.live_neighbor_count(row, col);
 
-                log!(
-                    "cell[{}, {}] is initially {:?} and has {} live neighbors",
-                    row,
-                    col,
-                    cell,
-                    live_neighbors
-                );
+                // log!(
+                //     "cell[{}, {}] is initially {:?} and has {} live neighbors",
+                //     row,
+                //     col,
+                //     cell,
+                //     live_neighbors
+                // );
 
                 let next_cell = match (cell, live_neighbors) {
                     // Rule 1: Any live cell with fewer than two live neighbors
@@ -95,7 +95,7 @@ impl Universe {
                     (otherwise, _) => otherwise,
                 };
 
-                log!("    it becomes {:?}", next_cell);
+                // log!("    it becomes {:?}", next_cell);
 
                 next[idx] = next_cell;
             }
