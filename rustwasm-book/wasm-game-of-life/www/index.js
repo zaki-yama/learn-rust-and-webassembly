9614118d2950574a1a9212d836f692126f4b75e7
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-09-04 09:36:27 +0900

    4.8 Time Profiling > Growing our Game of Life Universe

--- rustwasm-book/wasm-game-of-life/www/index.js
+++ rustwasm-book/wasm-game-of-life/www/index.js
@@ -106,12 +106,33 @@ const drawCells = () => {
 
   ctx.beginPath();
 
+  // Alive cells.
+  ctx.fillStyle = ALIVE_COLOR;
   for (let row = 0; row < height; row++) {
     for (let col = 0; col < width; col++) {
       const idx = getIndex(row, col);
 
-      ctx.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+      if (cells[idx] !== Cell.Alive) {
+        continue;
+      }
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
 
+  // Dead cells.
+  ctx.fillStyle = DEAD_COLOR;
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      if (cells[idx] !== Cell.Dead) {
+        continue;
+      }
       ctx.fillRect(
         col * (CELL_SIZE + 1) + 1,
         row * (CELL_SIZE + 1) + 1,
