9a96231ef38c1f1a1fdb6413d35b2ad0f8bfa89d
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-12-07 09:31:56 +0900

    7.5 モジュールを複数のファイルに分割する

--- the-book/restaurant/src/lib.rs
+++ the-book/restaurant/src/lib.rs
@@ -1,18 +1,4 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
 
 fn serve_order() {}
 
