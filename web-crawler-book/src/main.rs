defae61088e09a2cecf3b0cd848829510ac7a385
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-12-09 18:00:47 +0900

    ch03: <a>要素の抽出

--- web-crawler-book/src/main.rs
+++ web-crawler-book/src/main.rs
@@ -1,6 +1,13 @@
-fn main() -> eyre::Result<()> {
-    let body = reqwest::blocking::get("https://www.rust-lang.org")?.text();
-    println!("body = {:?}", body);
+use select::document::Document;
+use select::predicate::Name;
 
+fn main() -> eyre::Result<()> {
+    let body = reqwest::blocking::get("https://www.rust-lang.com")?.text()?;
+
+    let doc = Document::from(body.as_str());
+
+    for a in doc.find(Name("a")) {
+        println!("{:?}", a);
+    }
     Ok(())
 }
