96c2e296d6dc8cc7ab53072662e1badb7037bcfb
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2022-04-08 20:45:37 +0900

    [p80] ディスクマネージャ（本に掲載されてなかった部分）

--- web-db-press-122-rdbms/relly/src/disk.rs
+++ web-db-press-122-rdbms/relly/src/disk.rs
@@ -1,6 +1,41 @@
-use std::{fs::{File, OpenOptions}, io, path::Path};
+use std::{
+    fs::{File, OpenOptions},
+    io::{self, prelude::*, SeekFrom},
+    path::Path,
+};
 
+pub const PAGE_SIZE: usize = 4096;
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 pub struct PageId(pub u64);
+
+impl PageId {
+    pub const INVALID_PAGE_ID: PageId = PageId(u64::MAX);
+    pub fn valid(self) -> Option<PageId> {
+        if self == Self::INVALID_PAGE_ID {
+            None
+        } else {
+            Some(self)
+        }
+    }
+
+    pub fn to_u64(self) -> u64 {
+        self.0
+    }
+}
+
+impl Default for PageId {
+    fn default() -> Self {
+        PageId::INVALID_PAGE_ID
+    }
+}
+
+impl From<Option<PageId>> for PageId {
+    fn from(page_id: Option<PageId>) -> Self {
+        page_id.unwrap_or_default()
+    }
+}
+
 pub struct DiskManager {
     // ヒープファイルのファイルディスクリプタ
     heap_file: File,
@@ -10,29 +45,34 @@ pub struct DiskManager {
 
 impl DiskManager {
     // コンストラクタ
-    pub fn new(data_file: File) -> io::Result<Self> {
-        // (省略)
+    pub fn new(heap_file: File) -> io::Result<Self> {
+        let heap_file_size = heap_file.metadata()?.len();
+        let next_page_id = heap_file_size / PAGE_SIZE as u64;
+        Ok(Self {
+            heap_file,
+            next_page_id,
+        })
     }
 
     // ファイルパスを指定して開く
     pub fn open(heap_file_path: impl AsRef<Path>) -> io::Result<Self> {
         let heap_file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .open(heap_file_path)?;
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(heap_file_path)?;
         Self::new(heap_file)
     }
 
     // 新しいページIDを採番する
-    pub fn allocate_page(&mut Self) -> PageId {
+    pub fn allocate_page(&mut self) -> PageId {
         let page_id = self.next_page_id;
         self.next_page_id += 1;
         PageId(page_id)
     }
 
     // ページのデータを読み出す
-    pub fn read_page_data(&mut Self, page_id:PageId, data: &mut [u8]) -> io::Result<()>{
+    pub fn read_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> io::Result<()> {
         // オフセットを計算
         let offset = PAGE_SIZE as u64 * page_id.to_u64();
         // ページ先頭へシーク
@@ -42,12 +82,43 @@ impl DiskManager {
     }
 
     // データをページに書き出す
-    pub fn write_page_data(&mut Self, page_id: PageId, data: &[u8])->io::Result<()>{
+    pub fn write_page_data(&mut self, page_id: PageId, data: &[u8]) -> io::Result<()> {
         // オフセットを計算
-        let offset = PAGE_SIZE as u64 *page_id.to_u64();
+        let offset = PAGE_SIZE as u64 * page_id.to_u64();
         // ページ先頭へシーク
         self.heap_file.seek(SeekFrom::Start(offset))?;
         // データを書き込む
         self.heap_file.write_all(data)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempfile::NamedTempFile;
+
+    #[test]
+    fn test() {
+        let (data_file, data_file_path) = NamedTempFile::new().unwrap().into_parts();
+        let mut disk = DiskManager::new(data_file).unwrap();
+        let mut hello = Vec::with_capacity(PAGE_SIZE);
+        hello.extend_from_slice(b"hello");
+        hello.resize(PAGE_SIZE, 0);
+        let hello_page_id = disk.allocate_page();
+        disk.write_page_data(hello_page_id, &hello).unwrap();
+
+        let mut world = Vec::with_capacity(PAGE_SIZE);
+        world.extend_from_slice(b"world");
+        world.resize(PAGE_SIZE, 0);
+        let world_page_id = disk.allocate_page();
+        disk.write_page_data(world_page_id, &world).unwrap();
+
+        drop(disk);
+        let mut disk2 = DiskManager::open(&data_file_path).unwrap();
+        let mut buf = vec![0; PAGE_SIZE];
+        disk2.read_page_data(hello_page_id, &mut buf).unwrap();
+        assert_eq!(hello, buf);
+        disk2.read_page_data(world_page_id, &mut buf).unwrap();
+        assert_eq!(world, buf);
     }
 }
