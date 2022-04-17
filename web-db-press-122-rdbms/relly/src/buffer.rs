2f29d2528f0af4aa187234c39480471337e4d85d
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2022-04-18 01:18:31 +0900

    [p84] 第3章 バッファプールマネージャの実装

---
+++ web-db-press-122-rdbms/relly/src/buffer.rs
@@ -0,0 +1,127 @@
+use std::{
+    cell::{Cell, RefCell},
+    collections::HashMap,
+    ops::{Index, IndexMut},
+    rc::Rc,
+};
+
+use crate::disk::{DiskManager, PageId, PAGE_SIZE};
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] io::Error),
+    #[error("no free buffer available in buffer pool")]
+    NoFreeBuffer,
+}
+
+#[derive(Debug, Default, Clone, Copy, Eq, PartialEq, Hash)]
+pub struct BufferId(usize);
+
+pub type Page = [u8; PAGE_SIZE];
+
+pub struct Buffer {
+    pub page_id: PageId,
+    pub page: RefCell<Page>,
+    pub is_dirty: Cell<bool>,
+}
+
+pub struct Frame {
+    usage_count: u64,
+    buffer: Rc<Buffer>,
+}
+
+pub struct BufferPool {
+    buffers: Vec<Frame>,
+    next_victim_id: BufferId,
+}
+
+impl BufferPool {
+    fn size(&self) -> usize {
+        self.buffers.len()
+    }
+
+    /// 捨てるバッファを決めて、そのバッファIDを返す
+    /// すべてのバッファが貸出中で、捨てられるバッファが1つもなかった場合はNoneを返す
+    fn evict(&mut self) -> Option<BufferId> {
+        let pool_size = self.size();
+        let mut consecutive_pinned = 0;
+
+        let victim_id = loop {
+            let next_victim_id = self.next_victim_id;
+            let frame = &mut self[next_victim_id];
+            if frame.usage_count == 0 {
+                break self.next_victim_id;
+            }
+            // バッファが貸出中でない場合
+            if Rc::get_mut(&mut frame.buffer).is_some() {
+                frame.usage_count -= 1;
+                consecutive_pinned = 0;
+            } else {
+                consecutive_pinned += 1;
+                if consecutive_pinned >= pool_size {
+                    return None;
+                }
+            }
+            self.next_victim_id = self.increment_id(self.next_victim_id);
+        };
+        Some(victim_id)
+    }
+
+    fn increment_id(&self, buffer_id: BufferId) -> BufferId {
+        BufferId((buffer_id.0 + 1) % self.size())
+    }
+}
+
+impl Index<BufferId> for BufferPool {
+    type Output = Frame;
+
+    fn index(&self, index: BufferId) -> &Self::Output {
+        &self.buffers[index.0]
+    }
+}
+
+impl IndexMut<BufferId> for BufferPool {
+    fn index_mut(&mut self, index: BufferId) -> &mut Self::Output {
+        &mut self.buffers[index.0]
+    }
+}
+
+pub struct BufferPoolManager {
+    disk: DiskManager,
+    pool: BufferPool,
+    // ページテーブル
+    page_table: HashMap<PageId, BufferId>,
+}
+
+impl BufferPoolManager {
+    fn fetch_page(&mut self, page_id: PageId) -> Result<Rc<Buffer>, Error> {
+        if let Some(&buffer_id) = self.page_table.get(&page_id) {
+            let frame = &mut self.pool[buffer_id];
+            frame.usage_count += 1;
+            return Ok(frame.buffer.clone());
+        }
+
+        let buffer_id = self.pool.evict().ok_or(Error::NoFreeBuffer)?;
+        let frame = &mut self.pool[buffer_id];
+        let evict_page_id = frame.buffer.page_id;
+        {
+            let buffer = Rc::get_mut(&mut frame.buffer).unwrap();
+            // is_dirty フラグが立っている = バッファの内容が変更されており、ディスク上の内容が古くなっている
+            // => バッファをディスクに書き出す
+            if buffer.is_dirty.get() {
+                self.disk
+                    .write_page_data(evict_page_id, buffer.page.get_mut())?;
+            }
+            buffer.page_id = page_id;
+            buffer.is_dirty.set(false);
+            self.disk.read_page_data(page_id, buffer.page.get_mut())?;
+            frame.usage_count += 1;
+        }
+        let page = Rc::clone(&frame.buffer);
+        // ページテーブルを更新する
+        self.page_table.remove(&evict_page_id);
+        self.page_table.insert(page_id, buffer_id);
+        Ok(page)
+    }
+}
