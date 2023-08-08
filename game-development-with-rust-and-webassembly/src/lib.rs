c07e71581ccd2251c002658125b77f8f2777fafc
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2023-08-08 22:03:13 +0900

    [p48] 2.4 スプライトシートを用いた複数スプライトのロード

--- game-development-with-rust-and-webassembly/src/lib.rs
+++ game-development-with-rust-and-webassembly/src/lib.rs
@@ -2,9 +2,31 @@ use std::rc::Rc;
 use std::sync::Mutex;
 
 use rand::prelude::*;
+use serde::Deserialize;
+use std::collections::HashMap;
+use wasm_bindgen::convert::IntoWasmAbi;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
+
 use web_sys::console;
+
+#[derive(Deserialize)]
+struct Sheet {
+    frames: HashMap<String, Cell>,
+}
+
+#[derive(Deserialize)]
+struct Rect {
+    x: u16,
+    y: u16,
+    w: u16,
+    h: u16,
+}
+
+#[derive(Deserialize)]
+struct Cell {
+    frame: Rect,
+}
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -63,6 +85,50 @@ pub fn main_js() -> Result<(), JsValue> {
             [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
             (0, 255, 0),
             5,
+        );
+
+        let json = fetch_json("rhb.json")
+            .await
+            .expect("Could not fetch rhb.json");
+        let sheet: Sheet = json
+            .into_serde()
+            .expect("Could not convert rhb.json into a Sheet structure");
+
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = Rc::clone(&success_tx);
+
+        let image = web_sys::HtmlImageElement::new().unwrap();
+        let callback = Closure::once(move || {
+            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                success_tx.send(Ok(()));
+            }
+        });
+        let error_callback = Closure::once(move |err| {
+            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                error_tx.send(Err(err));
+            }
+        });
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
+        image.set_src("rhb.png");
+
+        success_rx.await;
+        let sprite = sheet.frames.get("Run (1).png").expect("Cell not found");
+        console::log_1(&sprite.frame.x.into());
+        console::log_1(&sprite.frame.y.into());
+        console::log_1(&sprite.frame.w.into());
+        console::log_1(&sprite.frame.h.into());
+        context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+            &image,
+            sprite.frame.x.into(),
+            sprite.frame.y.into(),
+            sprite.frame.w.into(),
+            sprite.frame.h.into(),
+            300.0,
+            300.0,
+            sprite.frame.w.into(),
+            sprite.frame.h.into(),
         );
     });
     Ok(())
@@ -129,4 +195,13 @@ fn sierpinski(
 
 fn midpoint(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
     ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0)
+}
+
+async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
+    let window = web_sys::window().unwrap();
+    // fetch_with_str は Promise を返し、それを JsFuture という Rust の Future に変換する
+    let resp_value = wasm_bindgen_futures::JsFuture::from(window.fetch_with_str(json_path)).await?;
+    let resp: web_sys::Response = resp_value.dyn_into()?;
+
+    wasm_bindgen_futures::JsFuture::from(resp.json()?).await
 }
