886be26223da0ab500982052fe157235622a6532
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2023-08-23 05:19:36 +0900

    [p85] 3.3.6 ゲームループの統合

---
+++ game-development-with-rust-and-webassembly/src/game.rs
@@ -0,0 +1,105 @@
+use std::collections::HashMap;
+
+use crate::{
+    browser,
+    engine::{self, Game, Rect, Renderer},
+};
+use anyhow::Result;
+use async_trait::async_trait;
+use serde::Deserialize;
+use web_sys::HtmlImageElement;
+
+#[async_trait(?Send)]
+impl Game for WalkTheDog {
+    async fn initialize(&self) -> Result<Box<dyn Game>> {
+        let sheet = browser::fetch_json("rhb.json").await?.into_serde()?;
+
+        let image = Some(engine::load_image("rhb.json").await?);
+
+        Ok(Box::new(WalkTheDog {
+            image,
+            sheet,
+            frame: self.frame,
+        }))
+    }
+
+    fn update(&mut self) {
+        // updateは1/60秒ごとに呼ばれる。
+        // アニメーションのタイミングをおおよそあわせるには、3回更新されたら1度スプライトを変更するようにしたい。
+        // Runアニメーションには8つのフレームがあるので、アニメーションが終わるのに24回のupdateが必要。
+        if self.frame < 23 {
+            self.frame += 1;
+        } else {
+            self.frame = 0;
+        }
+    }
+    fn draw(&self, renderer: &Renderer) {
+        let current_sprite = (self.frame / 3) + 1;
+        let frame_name = format!("Run ({}).png", current_sprite);
+
+        let sprite = self
+            .sheet
+            .as_ref()
+            .and_then(|sheet| sheet.frames.get(&frame_name))
+            .expect("Cell not found");
+
+        renderer.clear(&Rect {
+            x: 0.0,
+            y: 0.0,
+            width: 600.0,
+            height: 600.0,
+        });
+
+        self.image.as_ref().map(|image| {
+            renderer.draw_image(
+                &image,
+                &Rect {
+                    x: sprite.frame.x.into(),
+                    y: sprite.frame.y.into(),
+                    width: sprite.frame.w.into(),
+                    height: sprite.frame.h.into(),
+                },
+                &Rect {
+                    x: 300.0,
+                    y: 300.0,
+                    width: sprite.frame.w.into(),
+                    height: sprite.frame.h.into(),
+                },
+            )
+        });
+    }
+}
+
+#[derive(Deserialize)]
+struct SheetRect {
+    x: u16,
+    y: u16,
+    w: u16,
+    h: u16,
+}
+
+#[derive(Deserialize)]
+struct Cell {
+    frame: SheetRect,
+}
+
+#[derive(Deserialize)]
+pub struct Sheet {
+    frames: HashMap<String, Cell>,
+}
+
+pub struct WalkTheDog {
+    image: Option<HtmlImageElement>,
+    sheet: Option<Sheet>,
+    frame: u8,
+}
+
+impl WalkTheDog {
+    pub fn new() -> Self {
+        WalkTheDog {
+            image: None,
+            sheet: None,
+            frame: 0,
+        }
+    }
+}
--- game-development-with-rust-and-webassembly/src/lib.rs
+++ game-development-with-rust-and-webassembly/src/lib.rs
@@ -1,8 +1,11 @@
 #[macro_use]
 mod browser;
 mod engine;
+mod game;
 
 use anyhow::Result;
+use engine::GameLoop;
+use game::WalkTheDog;
 use std::rc::Rc;
 use std::sync::Mutex;
 
@@ -44,41 +47,11 @@ pub fn main_js() -> Result<(), JsValue> {
     let context = browser::context().expect("Could not get browser context");
 
     browser::spawn_local(async move {
-        let sheet: Sheet = browser::fetch_json("rhb.json")
-            .await
-            .expect("Could not fetch rhb.json")
-            .into_serde()
-            .expect("Could not convert rhb.json into a Sheet structure");
+        let game = WalkTheDog::new();
 
-        let image = engine::load_image("rhb.json")
+        GameLoop::start(game)
             .await
-            .expect("Could not load rhb.png");
-
-        let mut frame = -1;
-        let interval_callback = Closure::wrap(Box::new(move || {
-            frame = (frame + 1) % 8;
-            let frame_name = format!("Run ({}).png", frame + 1);
-            let sprite = sheet.frames.get(&frame_name).expect("Cell not found");
-            context.clear_rect(0.0, 0.0, 600.0, 600.0);
-            context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-                &image,
-                sprite.frame.x.into(),
-                sprite.frame.y.into(),
-                sprite.frame.w.into(),
-                sprite.frame.h.into(),
-                300.0,
-                300.0,
-                sprite.frame.w.into(),
-                sprite.frame.h.into(),
-            );
-        }) as Box<dyn FnMut()>);
-        browser::window()
-            .unwrap()
-            .set_interval_with_callback_and_timeout_and_arguments_0(
-                interval_callback.as_ref().unchecked_ref(),
-                50,
-            );
-        interval_callback.forget();
+            .expect("Could not start game loop");
     });
     Ok(())
 }
